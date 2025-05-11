168bab131725fecd593b63f0d696b61de5736551
Jonggu Lee <jg.jake.lee@gmail.com>
2025-05-11 15:44:02 +0900

    feat: Add IAM Policy

--- main.tf
+++ main.tf
@@ -13,3 +13,58 @@ module "vpc" {
 
   tags = var.default_tags
 }
+
+
+# IAM policy
+module "iam_policy_msk" {
+  source = "terraform-aws-modules/iam/aws//modules/iam-policy"
+
+  name        = "iotcore-test-msk-policy"
+  description = "IAM policy for IoT Core test"
+  policy      = <<EOF
+{
+   "Version": "2012-10-17",
+   "Statement": [
+      {
+         "Effect": "Allow",
+         "Action": [
+            "ec2:CreateNetworkInterface",
+            "ec2:DescribeNetworkInterfaces",
+            "ec2:CreateNetworkInterfacePermission",
+            "ec2:DeleteNetworkInterface",
+            "ec2:DescribeSubnets",
+            "ec2:DescribeVpcs",
+            "ec2:DescribeVpcAttribute",
+            "ec2:DescribeSecurityGroups"
+         ],
+         "Resource": "*"
+      },
+      {
+         "Effect": "Allow",
+         "Action": [
+            "secretsmanager:GetSecretValue",
+            "secretsmanager:DescribeSecret"
+         ],
+         "Resource": "arn:aws:secretsmanager:region:123456789012:secret:AmazonMSK_*"
+      }
+   ]
+}
+EOF
+
+  tags = var.default_tags
+}
+
+# IAM Role
+module "iam_role_msk" {
+  source = "terraform-aws-modules/iam/aws//modules/iam-assumable-role"
+
+  role_name             = "iotcore-test-msk-role"
+  create_role           = true
+  role_requires_mfa     = false
+  trusted_role_services = ["iot.amazonaws.com"]
+  trusted_role_actions  = ["sts:AssumeRole"]
+
+  custom_role_policy_arns = [module.iam_policy_msk.arn]
+
+  tags = var.default_tags
+}
